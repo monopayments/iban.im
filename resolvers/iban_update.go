85acba1e52178f7438452f1cb74ce7971ddcc09a
fakturk <f.akturk@gmail.com>
2020-01-19 17:53:06 +0300

    iban update

--- resolvers/iban_update.go
+++ resolvers/iban_update.go
@@ -32,19 +32,23 @@ func (r *Resolvers) IbanUpdate(ctx context.Context, args IbanUpdateMutationArgs)
 	iban := model.Iban{}
 	userid,_:= userID.(int)
 	ibans:=r.FindIbanByOwner(userid)
-	
+	iban = r.FindIbanByHandle(ibans,args.Handle)
 	fmt.Printf("ibans: %+v\n",ibans)
+	fmt.Printf("founded iban: %+v\n",ibans)
+
+	iban.Handle=args.Handle
+	iban.Text = args.Text
 
 	// if err := r.DB.First(&iban, ibanID).Error; err != nil {
 	// 	msg := "Not existing iban"
 	// 	return &IbanUpdateResponse{Status: false, Msg: &msg, Iban: nil}, nil
 	// }
 
-	// if args.Password != "" {
-	// 	iban.Password = args.Password
-	// }
+	if args.Password != "" {
+		iban.Password = args.Password
+	}
 
-	// r.DB.Save(&iban)
+	r.DB.Save(&iban)
 	return &IbanUpdateResponse{Status: true, Msg: nil, Iban: &IbanResponse{i: &iban}}, nil
 }
 
@@ -70,7 +74,17 @@ func (r *IbanUpdateResponse) Ok() bool {
 func (r *IbanUpdateResponse) Error() *string {
 	return r.Msg
 }
+func (r *Resolvers) FindIbanByHandle(ibans []model.Iban, handle string )  model.Iban{
+	for _,iban := range ibans{
+		fmt.Println(iban.Handle)
+		if handle == iban.Handle{
+			fmt.Println("Same handle found")
+			return iban
+		}
 
+	}
+	return model.Iban{}
+}
 func (r *Resolvers)FindIbanByOwner(userID int)[]model.Iban{
 	ibans:=[]model.Iban{}
 	// Get all matched records
