a49527f806d3bbb9ebef32196ca5b5703c8dda98
ahmetlutfuyazici <l6504167@std.yildiz.edu.tr>
2020-03-25 14:03:21 +0300

    fixed change password error message

--- resolvers/change_password.go
+++ resolvers/change_password.go
@@ -32,7 +32,7 @@ func (r *Resolvers) ChangePassword(ctx context.Context, args changePasswordMutat
 	user.HashPassword()
 
 	if err := config.DB.Save(&user).Error;err != nil {
-		msg := "Not existing user"
+		msg := err.Error()
 		return &ChangePasswordResponse{Status: false, Msg: &msg, User: nil}, err
 	}
 	return &ChangePasswordResponse{Status: true, Msg: nil, User: &UserResponse{u: &user}}, nil
