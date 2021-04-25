cbaa3302a5e3d95d3d96b0b91f5601a1c2f8f30c
Hüseyin Mert <huseyinin@gmail.com>
2021-04-25 19:08:53 +0300

    now we have a about page

---
+++ web/src/components/About.vue
@@ -0,0 +1,22 @@
+<template>
+    <div class="box">
+        <h1>About IBAN.im</h1>
+        <p>I feel that I could never memorize numbers.<br />
+
+        If you need to share it, it becomes a huge pain. You need to login your bank and copy if you can. Then share it via sms or WhatsApp. Then wait for next pain. Bank applications are hard when you have to copy/paste something.</p>
+        <p>This service is build by <a href="https://monopayments.com">MonoPayments</a>.</p>
+    </div>
+</template>
+
+<script>
+
+    export default {
+        name: "About",
+        data: () => ({
+
+        }),
+        created() {
+            this.$store.state.isLoaded = true
+        }
+    }
+</script>
